21cdbf0a9e8176eef6b4069225fb9d981c7a6047
dinstone <dinstone@163.com>
2019-10-06 20:29:21 +0800

    jackson 2.10.0

--- focus-example/src/main/java/com/dinstone/focus/server/ServerTest.java
+++ focus-example/src/main/java/com/dinstone/focus/server/ServerTest.java
@@ -4,14 +4,19 @@ import java.io.IOException;
 
 import com.dinstone.focus.example.DemoService;
 import com.dinstone.focus.example.DemoServiceImpl;
+import com.dinstone.loghub.Logger;
+import com.dinstone.loghub.LoggerFactory;
 
 public class ServerTest {
+    private static final Logger LOG = LoggerFactory.getLogger(Server.class);
 
     public static void main(String[] args) {
         Server server = new Server(new ServerOptions().bind("localhost", 3333));
         server.exporting(DemoService.class, new DemoServiceImpl());
-        server.start();
 
+        long t = System.currentTimeMillis();
+        server.start();
+        LOG.info("server start take's {}ms", System.currentTimeMillis() - t);
         try {
             System.in.read();
         } catch (IOException e) {
